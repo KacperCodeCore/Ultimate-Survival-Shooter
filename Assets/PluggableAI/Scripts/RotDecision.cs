d2db894647f70b01c665b50071c371391193f835
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2023-10-29 16:12:14 +0100

    fix rot

--- Assets/PluggableAI/Scripts/RotDecision.cs
+++ Assets/PluggableAI/Scripts/RotDecision.cs
@@ -1,17 +1,23 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "PluggableAI/Decisions/RotDecision")]
 public class RotDecision : Decision
 {
-    public float rotationAngle = 360f;
+    public float targetRotationAngle = 360f; // Docelowy kąt obrotu, np. 30 stopni
+    private float currentRotation = 0f;
+
     public override bool Decide(StateController controller)
     {
-        if(controller.transform.rotation.eulerAngles.y >= rotationAngle)
+        currentRotation += controller.enemyStats.searchingTurnSpeed * Time.deltaTime;
+
+        if (currentRotation >= targetRotationAngle)
         {
+            currentRotation = 0f; // restart wartość obrotu po osiągnięciu docelowego kąta
             return false;
         }
+
         return true;
     }
 }
