0c7e6b6bf4473dc08d0ea407c63b649be232766b
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2023-11-29 02:42:41 +0100

    HealthSphere animation, destroy

---
+++ Assets/PluggableAI/Scripts/CollisonDrawLine.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDrawLine : MonoBehaviour
+{
+    [SerializeField] private Color _color;
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    ContactPoint contact = other.ClosestPoint(transform.position);
+    //    Debug.DrawLine(contact.point, contact.point + contact.normal, _color, 32, false);
+    //}
+}
--- Assets/PluggableAI/Scripts/Heal.cs
+++ Assets/PluggableAI/Scripts/Heal.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.Services.Analytics;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -6,24 +7,71 @@ public class Heal : MonoBehaviour
 {
     [SerializeField] private List<HeallableObjectType> _healedTyper = new List<HeallableObjectType>();
     [SerializeField] private List<IHealth> _healedObjects = new List<IHealth>();
-    [SerializeField] private float _healRate = 5f;
-    [SerializeField] private float _OneTimeHealAmount = 10.0f;
+    [SerializeField] private float _healRate = 0.01f;
+    [SerializeField] private float _OneTimeHealAmount = 4.0f;
+    [SerializeField] private int _MaxHealRepeat = 20;
 
+    private SphereCollider _sphereCollider;
     private float _timer = 0f;
 
+    private float _initialScale;
+
+    private void Awake()
+    {   
+        _sphereCollider = GetComponent<SphereCollider>();
+        _initialScale = transform.localScale.x;
+    }
     private void Update()
     {
-        _timer -= Time.deltaTime;
-        if (_timer < 0f && _healedObjects.Count > 0)
+        if (_healedObjects.Count > 0)
         {
-            _timer = _healRate;
             foreach (var item in _healedObjects)
             {
-                item.HealAmount(_OneTimeHealAmount);
+                if (item.CurrentHealth < item.MaxHealth)
+                {
+                    _initialScale -= 1f * Time.deltaTime;
+                    item.HealAmount(_OneTimeHealAmount * Time.deltaTime);
+                }
+                else
+                {
+                    _initialScale -= 0.2f * Time.deltaTime;
+                }
+                UpdateScale();
             }
         }
+        else
+        {
+            _initialScale -= 0.2f * Time.deltaTime;
+            UpdateScale();
+        }
+        if (_initialScale < 0f)
+        {
+            Destroy(gameObject);
+        }
+
+        //_timer -= Time.deltaTime;
+        //if (_timer < 0f && _healedObjects.Count > 0)
+        //{
+        //    _timer = _healRate;
+        //    _MaxHealRepeat--;
+        //    foreach (var item in _healedObjects)
+        //    {
+        //        item.HealAmount(_OneTimeHealAmount);
+        //        transform.localScale = new Vector3(_transform.localScale.x - 0.1f, _transform.localScale.y, _transform.localScale.z - 0.1f);
+        //    }
+        //    if(_MaxHealRepeat <= 0)
+        //    {
+        //        Destroy(gameObject);
+        //    }
+        //}
 
     }
+    private void UpdateScale()
+    {
+        transform.localScale = new Vector3(_initialScale, transform.localScale.y, _initialScale);
+        _sphereCollider.radius = _initialScale * 0.05f;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<IHealth>(out IHealth healableObject) &&
