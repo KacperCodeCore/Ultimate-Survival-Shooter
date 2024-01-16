95a220a8284841951332cbb2815afd87025b867e
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2024-01-16 22:10:00 +0100

    .

--- Assets/PluggableAI/Scripts/Action/Tower/RotateTowardsAction.cs
+++ Assets/PluggableAI/Scripts/Action/Tower/RotateTowardsAction.cs
@@ -12,8 +12,9 @@ public class RotateTowardsAction : Action
 
     private void RotateTowards(StateController controller)
     {
-        var target = controller.chaseTarget.position = controller.transform.position;
-        var newRot = Vector3.RotateTowards(controller.transform.forward, target, Mathf.Deg2Rad * controller.enemyStats.searchingTurnSpeed * Time.deltaTime, 0.0f);
+        var target = controller.chaseTarget.position - controller.transform.position;
+        var newRot = Vector3.RotateTowards(controller.transform.forward, target,
+            Mathf.Deg2Rad * controller.enemyStats.searchingTurnSpeed * Time.deltaTime, 0.0f);
         controller.transform.rotation = Quaternion.LookRotation(newRot);
     }
 }
